0fbba9c674d13d006a29fe1eae9daecf1716f1a9
Evgeny <dev@twave.ru>
2020-10-28 09:52:54 +0300

    First homework

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './app.modules.scss'
+import './color.css'
+
+const App = () => {
+  return (
+    <div className={cn(styles.h1, 'color')}>
+      This test text
+    </div>
+  )
+}
+
+export default App
