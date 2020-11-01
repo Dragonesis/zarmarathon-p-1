327fee0976403af9c978b3eb1f40897c1af76f0d
Evgeny <dev@twave.ru>
2020-11-01 11:37:01 +0300

    Homework 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import cn from 'classnames'
 import styles from './app.modules.scss'
 import './color.css'
 
+console.log(styles)
+
+
 const App = () => {
   return (
-    <div className={cn(styles.h1, 'color')}>
+    <div className={cn(styles.h1 , 'color')}>
       This test text
     </div>
   )
